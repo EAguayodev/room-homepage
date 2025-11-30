371a03443877021037ed5e4ef0806d30d7c1c49b
Eric Aguayo <computergeek90@protonmail.com>
2025-11-30 13:45:29 -0600

    remocing warnings from challenge submissions

--- js/page.js
+++ js/page.js
@@ -14,25 +14,25 @@ btnMenuToggle.addEventListener('click', function () {
 // slider to display hero 2 and 3 background image when user clicks on left or right arrow
 let currentIndex = 0;
 
-let headingText = [
+const headingText = [
     "Discover innovative ways to decorate",
     "We are available all across the globe",
     "Manufactured with the best materials"
 ];
 
-let textDescription = [
+const textDescription = [
     "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
     "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
 ];
 
-let heroBgImages = [
+const heroBgImages = [
   "images/desktop-image-hero-1.jpg",
   "images/desktop-image-hero-2.jpg",
   "images/desktop-image-hero-3.jpg",
 ];
 
-let heroBgImagesMobile = [
+const heroBgImagesMobile = [
   "images/mobile-image-hero-1.jpg",
   "images/mobile-image-hero-2.jpg",
   "images/mobile-image-hero-3.jpg",
@@ -53,7 +53,7 @@ function updateHeroContent(index) {
     if (window.innerWidth < 768) {
       heroSection.style.backgroundImage = `url('${heroBgImagesMobile[index]}')`;
     }
-    
+
     headingElement.textContent = headingText[index];
     descriptionElement.textContent = textDescription[index];
 
